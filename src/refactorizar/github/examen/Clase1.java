32949da7243127af8f48a97290df1134b55c9bdf
Iván Martínez Albalá <>
2021-02-23 13:19:17 +0100

    Control Github 1ª versión

---
+++ src/refactorizar/github/examen/Clase1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refactorizar.github.examen;
+
+/**
+ *
+ * @author Ivan Martínez Albalá
+ */
+public class Clase1 {
+    
+    public void Inicializar(){
+    System.out.println("Esta es la primera linea");
+    System.out.println("Esta es mi segunda linea");
+    }
+}
---
+++ src/refactorizar/github/examen/RefactorizarGithubExamen.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refactorizar.github.examen;
+
+/**
+ *
+ * @author dam
+ */
+public class RefactorizarGithubExamen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
