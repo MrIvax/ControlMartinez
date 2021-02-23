93ebe5d7d3e209948f64aeea966a62af62f08ba5
Iván Martínez Albalá <dam@203P01>
2021-02-23 13:24:55 +0100

    Control Github 2ª versión

---
+++ src/refactorizar/github/examen/Clase2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refactorizar.github.examen;
+
+/**
+ *
+ * @author Iván Martínez Albalá
+ */
+public class Clase2 {
+    public void Inicializar(){
+        System.out.println("Esta es la primera línea de la clase 2");
+        System.out.println("Esta es la segunda línea de la clase 2");
+    }
+}
